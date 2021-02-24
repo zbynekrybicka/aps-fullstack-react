e745e6a41aedef03dd4e0e4688e5815a0cb87864
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2021-02-25 00:32:46 +0100

    test correction

--- app/Aps.php
+++ app/Aps.php
@@ -27,39 +27,39 @@ class Aps
     {
         foreach ($data->components as $component) {
             $this->saveTemplate(
-                __DIR__ . '/templates/component.php',
+                __DIR__ . '/Templates/component.php',
                 __DIR__ . '/../dist/front/src/components/' . $component->title . '.js',
                 (array) $component);
         }
         
         $this->saveTemplate(
-            __DIR__ . '/templates/store.php', 
+            __DIR__ . '/Templates/store.php', 
             __DIR__ . '/../dist/front/src/store/index.js', 
             [ 'reducers' => $data->reducers ]
         );
         
         $this->saveTemplate(
-            __DIR__ . '/templates/state.php', 
+            __DIR__ . '/Templates/state.php', 
             __DIR__ . '/../dist/front/src/store/state.js', 
             [ 'state' => $data->state ]
         );
 
         foreach ($data->actions as $action) {
             $this->saveTemplate(
-                __DIR__ . '/templates/action.php',
+                __DIR__ . '/Templates/action.php',
                 __DIR__ . '/../dist/front/src/actions/' . $action->title . '.js',
                 (array) $action);
         }
 
         $this->saveTemplate(
-            __DIR__ . '/templates/route.php', 
+            __DIR__ . '/Templates/route.php', 
             __DIR__ . '/../dist/api/app/route.php', 
             [ 'routes' => $data->routes ]
         );
         
         foreach ($data->services as $service) {
             $this->saveTemplate(
-                __DIR__ . '/templates/service.php',
+                __DIR__ . '/Templates/service.php',
                 __DIR__ . '/../dist/api/app/Service/' . ucfirst($service->title) . 'Service.php',
                 (array) $service);
         }
